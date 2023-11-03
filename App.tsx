54feed96da1ff7178748af2aca2871bc9eea82bb
ihorkozar <i.kozar97@gmail.com>
2023-11-03 11:35:25 +0200

    wip

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import {StatusBar} from "expo-status-bar";
-import {StyleSheet, View} from "react-native";
+import {StyleSheet, View, Platform} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import IconButton from "./components/IconButton";
 import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
 import EmojiSticker from "./components/EmojiSticker";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import {captureRef} from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -18,6 +21,7 @@ export default function App() {
     const [showAppOptions, setShowAppOptions] = useState(false);
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [pickedEmoji, setPickedEmoji] = useState(null);
+    const [status, requestPermission] = MediaLibrary.usePermissions();
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -46,18 +50,54 @@ export default function App() {
     };
 
     const onSaveImageAsync = async () => {
-        // we will implement this later
+        if (Platform.OS !== 'web') {
+            try {
+                const localUri = await captureRef(imageRef, {
+                    height: 440,
+                    quality: 1,
+                });
+                await MediaLibrary.saveToLibraryAsync(localUri);
+                if (localUri) {
+                    alert('Saved!');
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            try {
+                const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+                    quality: 0.95,
+                    width: 320,
+                    height: 440,
+                });
+
+                let link = document.createElement('a');
+                link.download = 'sticker-smash.jpeg';
+                link.href = dataUrl;
+                link.click();
+            } catch (e) {
+                console.log(e);
+            }
+        }
     };
+
+    if (status === null) {
+        requestPermission();
+    }
+
+    const imageRef = useRef();
 
     return (
         <GestureHandlerRootView style={styles.container}>
             <View style={styles.container}>
                 <View style={styles.imageContainer}>
-                    <ImageViewer
-                        placeholderImageSource={PlaceholderImage}
-                        selectedImage={selectedImage}
-                    />
-                    {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> : null}
+                    <View ref={imageRef} collapsable={false}>
+                        <ImageViewer
+                            placeholderImageSource={PlaceholderImage}
+                            selectedImage={selectedImage}
+                        />
+                        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> : null}
+                    </View>
                 </View>
                 {showAppOptions ? (
                     <View style={styles.optionsContainer}>
@@ -76,7 +116,7 @@ export default function App() {
                 <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
                     <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
                 </EmojiPicker>
-                <StatusBar style="auto"/>
+                <StatusBar style="light"/>
             </View>
         </GestureHandlerRootView>
     );
